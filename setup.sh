6e0908cbd84a0cb1f84f863184c83f0bdb55e3da
Lukas Herman <lukashh6@gmail.com>
2017-03-19 19:38:17 -0400

    Add 'source' after aliasing

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ INSTL_PATH="$(pwd)"
 # Create an alias in .bash_profile
 echo "Creating a new alias"
 echo "alias tasks='cd ${INSTL_PATH} && ./run.sh $*'" >> $HOME/.bash_profile
+source $HOME/.bash_profile
 
 echo "Done!"
 
