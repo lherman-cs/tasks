61393ec24416477ed4102ac0aeb0da72a54817f1
Lukas Herman <lukashh6@gmail.com>
2017-03-19 19:46:24 -0400

    Hotfix setup script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ INSTL_PATH="$(pwd)"
 
 # Create an alias in .bash_profile
 echo "Creating a new alias"
-echo "alias tasks='cd ${INSTL_PATH} && ./run.sh $*'" >> $HOME/.bash_profile
+echo "alias tasks='cd ${INSTL_PATH} && ./run.sh'" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 echo "Done!"
