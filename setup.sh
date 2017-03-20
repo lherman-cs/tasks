2dbc05b75292e4bf60c1d2e7ea868dac73a19744
Lukas Herman <lukashh6@gmail.com>
2017-03-20 07:22:50 -0400

    Fix duplicated settings.py

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ SRC_PATH="${INSTL_PATH}/src"
 LOG_PATH="${INSTL_PATH}/log"
 
 # Generate settings.py
-echo "TASKS_PATH = '${LOG_PATH}/tasks.pkl'" >> ${SRC_PATH}/settings.py 
+echo "TASKS_PATH = '${LOG_PATH}/tasks.pkl'" > ${SRC_PATH}/settings.py 
 echo "LOG_PATH = '${LOG_PATH}/log.txt'" >> ${SRC_PATH}/settings.py 
 echo "MARGIN = 80" >> ${SRC_PATH}/settings.py 
 
