0c66894eac4a9d1800c76a35440c907fe981ed70
Lukas Herman <lukashh6@gmail.com>
2017-03-21 07:01:02 -0400

    Rewrite

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import pickle
 from task import Task
 from sys import stderr
 import settings
+import re
 
 tasks_path = settings.TASKS_PATH
 log_path = settings.LOG_PATH
@@ -42,7 +43,7 @@ def print_tasks():
 
     index = 1
     for task in tasks:
-        print('\t{}. {}'.format(index, task.task))
+        print('\t{}. {}'.format(index, task.get_info()))
         index += 1
 
 def add(task):
@@ -51,11 +52,33 @@ def add(task):
     tasks.append(Task(task))
     save(tasks)
 
-def done(task_n=1):
+# Set the completion percertange on task_n
+def set(task_n=1):
     tasks = load()
     if tasks:
+        # Make sure the tasks_n is in range
         if 1 <= task_n <= len(tasks):
-            print('{} - Done'.format(tasks.pop(task_n-1).task))
+            task = tasks[task_n-1]
+            print(' Setting a new completion percetange for {} '.format(task.get_info())\
+                   .center(margin, '='))
+
+            # Make sure the input value is valid
+            pattern = re.compile('^\d+')
+            completion = input("Input a new value: ")
+            if not pattern.match(completion):
+                completion = input("Please give a valid value")
+
+            # Make sure the completion percentage is in range
+            completion = float(completion)
+            if completion >= 100:
+                task.completion = completion
+                print("Congratulation you've finished your task")
+                print("{}".format(task.get_info()))
+                tasks.pop(task_n-1)
+            else:
+                task.completion = 0 if completion < 0 else completion
+                print("Task #{} has been set a new completion percentage!".format(task_n))
+                print(task.get_info())
         else:
             print('Please give a valid index')
     else:
@@ -67,20 +90,16 @@ def main():
     gparser = parser.add_argument_group()
     gparser.add_argument('-a', '--add', dest='task', help='Add a task', \
                           type=str)
-    gparser.add_argument('-d', '--done', help="Remove the first task", \
-                          action='store_true')
-    gparser.add_argument('-f', '--finish', help="Remove the task_n", \
-                          type=int, dest='task_n')
+    gparser.add_argument('-s', '--set', dest='task_n', type=int, \
+                          help="Set completion to task_n in percentage")
 
 
     args = parser.parse_args()
 
     if args.task is not None:
         add(args.task)
-    elif args.done:
-        done()
     elif args.task_n:
-        done(args.task_n)
+        set(args.task_n)
     else:
         print_tasks()
 
--- src/task.py
+++ src/task.py
@@ -1,3 +1,7 @@
 class Task:
-    def __init__(self, task):
+    def __init__(self, task, completion=0):
         self.task = task
+        self.completion = completion
+
+    def get_info(self):
+        return "{} - {}%".format(self.task, self.completion)
